69f2935a7cd0dd94dea00c4e1dd0b8c2cd762c7d
Eliana Cepeda <eli.cepedagonzalez@gmail.com>
2024-08-08 07:24:45 -0500

    Cambio de nombre de carpeta 'primeros ejercicios en c' por 'Cap01'

---
+++ Cap01/example1.c
@@ -0,0 +1,28 @@
+/*******************************************************************
+Autor: Eliana Cepeda
+Materia: Sistemas Operativos
+Fecha: 18-07-2024
+Tema: Introducción al lenguaje c y los entornos
+  -Entornos en la web (CLOUD/NUBE)
+  - Comandos principales de linux
+  -Programa 1: Presentación
+********************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  printf("**********************************************************************************************\n");
+  printf("\t PRESENTACIÓN \\ PRIMERA ENTRAGA\n\n");
+  printf("Nombre: \t Eliana Katherine Cepeda Gonzalez \n");
+  printf("Carreras: \t Ingenería de sitemas, Ingeniería Civil\n");
+  printf("Materias: \t 7\n");
+  printf("\t\t - Mecanica Estructural 3\n");
+  printf("\t\t - Pensamiento Sistemico\n");
+  printf("\t\t - Fundamentos en seguridad de la información\n");
+  printf("\t\t - Gestión en proyectos de TI\n");
+  printf("\t\t - Sistemas Operativos!!\n");
+  printf("\t\t - Estructuras de datos\n");
+  printf("\t\t - Constitución y derecho civil\n");
+  printf("**********************************************************************************************\n");
+  return 0;
+}
